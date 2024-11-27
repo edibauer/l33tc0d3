42e93758e6cb1dc87de6b6f106844cb6b4dbd4e9
edibaldo <edibaldo_369@proton.me>
2024-11-26 23:01:58 -0600

    Feat: Solving SQRT without using sqrt functions in java exercise

---
+++ src/Sqrt/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+        You must not use any built-in exponent function or operator.
+
+        For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+        
+
+        Example 1:
+
+        Input: x = 4
+        Output: 2
+        Explanation: The square root of 4 is 2, so we return 2.
+        Example 2:
+
+        Input: x = 8
+        Output: 2
+        Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+        
+
+        Constraints:
+
+        0 <= x <= 231 - 1
+         */
+        int num = 8;
+        int counter = 0;
+        int prime = 1;
+
+        
+        while (num > 0 && num >= prime) {
+            num = num - prime;
+            counter = counter + 1;
+            prime += 2;
+        }
+
+        System.out.println(counter);
+    }
+}
